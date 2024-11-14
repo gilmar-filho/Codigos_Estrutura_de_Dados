2b85eaf506318076627814c0907e62354876cf5e
gilmar-filho <gfilho416@gmail.com>
2024-11-14 11:27:32 -0300

    código hash corrigido e adição de código do chat

--- Hash/hash_chat.cpp
+++ Hash/hash_chat.cpp
@@ -15,7 +15,7 @@ class noh {
         char tipo;
         int valor;
         noh* proximo;
-        noh(const string& c = "", const char& t = 'a', const int& v=0, noh* prox = NULL):
+        noh(const string& c = "", const char& t = 'a', const int& v=0, noh* prox = nullptr):
             chave(c), tipo(t), valor(v), proximo(prox) { }
 };
 
@@ -25,7 +25,7 @@ class lista {
     private:
         noh* primeiro; // primeiro nó da lista
         unsigned short numElementos; // quantidade de valores na lista
-        void inserirVazia(const string& c, const char& t,const int& v);
+        void inserirVazia(const string& c, const char& t, const int& v);
     public:
         lista(); // Construtor
         ~lista(); // Destrutor
@@ -33,7 +33,7 @@ class lista {
         unsigned short tamanho() { return numElementos; } // Consulta do Tamanho
         // Adiciona um novo par (chave, valor) na lista
         // Insere no início, para não precisar de ponteiro para o fim
-        void insere(const string& c, const char& t,const int& v);
+        void insere(const string& c, const char& t, const int& v);
         // Remove o noh com dada chave da lista.
         // Se a chave não existe, retorna informação.
         bool remove(const string& c);
@@ -47,177 +47,158 @@ class lista {
         // Imprime o conteúdo da lista, para fins de depuração
         void imprime() {
             noh* atual = primeiro;
-            while (atual != NULL) {
+            while (atual != nullptr) {
                     cout << "[" << atual->chave << "/"
-                         << atual->valor << "]";
+                         << atual->valor << "] ";
                     atual = atual->proximo;
             }
         }
 
-        bool vazia(){ return (primeiro == NULL); }
+        bool vazia() { return (primeiro == nullptr); }
 };
 
-lista::lista(){
-    primeiro = NULL;
+lista::lista() {
+    primeiro = nullptr;
     numElementos = 0;
 }
 
-lista::~lista(){
+lista::~lista() {
     removeTodos();
 }
 
-void lista::removeTodos(){
+void lista::removeTodos() {
     noh* aux = primeiro;
-    while (aux != NULL){
+    while (aux != nullptr) {
         noh* temp = aux;
         aux = aux->proximo;
         delete temp;
     }
-    
-    primeiro = NULL;
+    primeiro = nullptr;
     numElementos = 0;
 }
 
-void lista::inserirVazia(const string& c, const char& t,const int& v){
-    noh* novo = new noh(c,t,v);
+void lista::inserirVazia(const string& c, const char& t, const int& v) {
+    noh* novo = new noh(c, t, v);
     primeiro = novo;
     numElementos++;
 }
 
-void lista::insere(const string& c, const char& t,const int& v) {
-    if (vazia()) inserirVazia(c,t,v);
+void lista::insere(const string& c, const char& t, const int& v) {
+    if (vazia()) inserirVazia(c, t, v);
     else {
-        noh* novo = new noh(c,t,v);
+        noh* novo = new noh(c, t, v);
         novo->proximo = primeiro;
         primeiro = novo;
         numElementos++;
     }
 }
 
-bool lista::remove(const string& c){
-    if (vazia()) throw runtime_error("Erro: hash vazia!");
+bool lista::remove(const string& c) {
+    if (vazia()) throw runtime_error("Erro: lista vazia!");
     else {
-        noh* aux = primeiro;
-        noh* anterior = NULL;
+        noh* atual = primeiro;
+        noh* anterior = nullptr;
 
-        while ((aux != NULL) and (aux->chave != c)){
-            anterior = aux;
-            aux = aux->proximo;
+        while (atual != nullptr && atual->chave != c) {
+            anterior = atual;
+            atual = atual->proximo;
         }
 
-        if (aux == NULL) return false;
-        if (anterior == NULL) primeiro = aux->proximo;
-        else anterior->proximo = aux->proximo;
+        if (atual == nullptr) return false;
+        if (anterior == nullptr) primeiro = atual->proximo;
+        else anterior->proximo = atual->proximo;
 
-        delete aux;
+        delete atual;
         numElementos--;
-
         return true;
     }
 }
 
-bool lista::busca(const string& c, char& tipoBuscado, int& valorBuscado){
-    if (vazia()) throw runtime_error("Erro: hash vazia!");
+bool lista::busca(const string& c, char& tipoBuscado, int& valorBuscado) {
+    if (vazia()) throw runtime_error("Erro: lista vazia!");
     else {
         noh* aux = primeiro;
-
-        while (aux != NULL){
+        while (aux != nullptr) {
             if (aux->chave == c) {
                 tipoBuscado = aux->tipo;
                 valorBuscado = aux->valor;
                 return true;
             }
-            else aux = aux->proximo;
+            aux = aux->proximo;
         }
-
-        cout << "Elemento inexistente!" << endl;
         return false;
     }
 }
 
 bool lista::verificaOcorrencia(const string& c) {
     noh* aux = primeiro;
-
-    while (aux != NULL){
+    while (aux != nullptr) {
         if (aux->chave == c) return true;
-        else aux = aux->proximo;
+        aux = aux->proximo;
     }
-
     return false;
 }
 
 class tabelaHash {
     private:
-        // vetor de ponteiros para lista
-        lista* *tabela;
-        // quantidade de posições na tabela hash
-        unsigned numPosicoes;
-        // converte uma chave (string) num endereço da tabela 
-        // - versão simples
-        unsigned funcaoHash(const string& s) {
-            return funcaoHash(s, numPosicoes);
-        } 
-        // converte uma chave (string) num endereço da tabela 
-        // - versão para redimensionamento
-        unsigned funcaoHash(const string& s, int cap) {
-            unsigned pos = 0;
-            for (unsigned int i = 0; i < s.size(); ++i)
-                pos = (UMPRIMO * pos + s[i]) % cap;
-            return pos;
-        }
+        lista** tabela; // vetor de ponteiros para lista
+        unsigned numPosicoes; // quantidade de posições na tabela hash
+        unsigned funcaoHash(const string& s) const; // converte uma chave (string) num endereço da tabela
     public:
-        // construtor padrão
-        tabelaHash(unsigned cap = 100);
+        tabelaHash(unsigned cap = 100); // construtor padrão
         ~tabelaHash();
-        // Insere um nó com dada chave e valor.
-        bool insere(const string& c, char& t, const int& v);
-        // Retorna um valor associado a uma dada chave.
-        // Se a chave não existe, retorna "NÃO ENCONTRADO!".
+        bool insere(const string& c, const char t, const int v);
         bool valor(const string& c, char& tipoBuscado, int& valorRetorno);
-        // Retira do hash um nó com dada chave.
         bool remove(const string& c);
-        // Imprime o conteúdo interno do hash (para fins de depuração)
-        void imprime() {
-            for (unsigned i = 0; i < numPosicoes; i++) {
-                cout << i << ": ";
-                tabela[i]->imprime();
-                cout << endl;
-            }
-        }
+        void imprime() const;
 };
 
-tabelaHash::tabelaHash(unsigned cap){
+unsigned tabelaHash::funcaoHash(const string& s) const {
+    unsigned pos = 0;
+    for (unsigned int i = 0; i < s.size(); ++i)
+        pos = (UMPRIMO * pos + s[i]) % numPosicoes;
+    return pos;
+}
+
+tabelaHash::tabelaHash(unsigned cap) {
     tabela = new lista*[cap];
     for (unsigned i = 0; i < cap; i++) tabela[i] = new lista();
     numPosicoes = cap;
 }
 
-tabelaHash::~tabelaHash(){
-    for (unsigned i = 0; i < numPosicoes; i++){
-        tabela[i]->removeTodos();
+tabelaHash::~tabelaHash() {
+    for (unsigned i = 0; i < numPosicoes; i++) {
         delete tabela[i];
     }
-    numPosicoes = 0;
+    delete[] tabela;
 }
 
-bool tabelaHash::insere(const string& c, char& t, const int& v){
+bool tabelaHash::insere(const string& c, const char t, const int v) {
     unsigned pos = funcaoHash(c);
     if (!tabela[pos]->verificaOcorrencia(c)) {
-        tabela[pos]->insere(c,t,v);
-        cout << "Chave '" << c << "' inserida na posicao " << pos << endl;
+        tabela[pos]->insere(c, t, v);
         return true;
-    } else return false;
-    
+    } else {
+        return false;
+    }
 }
 
-bool tabelaHash::valor(const string& c, char& tipoBuscado, int& valorRetorno){
+bool tabelaHash::valor(const string& c, char& tipoBuscado, int& valorRetorno) {
     unsigned pos = funcaoHash(c);
-    return tabela[pos]->busca(c,tipoBuscado,valorRetorno);
+    return tabela[pos]->busca(c, tipoBuscado, valorRetorno);
 }
 
 bool tabelaHash::remove(const string& c) {  
     unsigned pos = funcaoHash(c);
     return tabela[pos]->remove(c);
+}
+
+void tabelaHash::imprime() const {
+    for (unsigned i = 0; i < numPosicoes; i++) {
+        cout << i << ": ";
+        tabela[i]->imprime();
+        cout << endl;
+    }
 }
 
 int main() {
@@ -226,25 +207,25 @@ int main() {
     tabelaHash tabela(tamanhoTH);
     char comando;
     string chave;
-    char tipo = 'a';
-    int valor = -1;    
+    char tipo;
+    int valor;    
     do {
-        try{
+        try {
             cin >> comando;
             switch (comando) {
                 case 'i': // inserir
-                    cin >> chave >> tipo>> valor;
-                    if (not tabela.insere(chave, tipo, valor))
+                    cin >> chave >> tipo >> valor;
+                    if (!tabela.insere(chave, tipo, valor))
                         cout << "Erro na inserção: chave já existente!" << endl;
                     break;
                 case 'r': // remover
                     cin >> chave;
-                    if (not tabela.remove(chave))
+                    if (!tabela.remove(chave))
                         cout << "Erro na remoção: chave não encontrada!" << endl;
                     break;
-                case 'l': // remover
+                case 'l': // busca
                     cin >> chave;
-                    if (not tabela.valor(chave,tipo,valor))
+                    if (!tabela.valor(chave, tipo, valor))
                         cout << "Erro na busca: chave não encontrada!" << endl;
                     else
                         cout << "Tipo: " << tipo << " Valor: " << valor << endl;
@@ -252,18 +233,14 @@ int main() {
                 case 'p': // mostrar estrutura
                     tabela.imprime();
                     break;
-
                 case 'f': // finalizar
-                    // checado no do-while
                     break;
                 default:
-                    cerr << "comando inválido\n";
+                    cout << "Comando inválido\n";
             }
         } catch (runtime_error& e) {
             cout << e.what() << endl;
         }
     } while (comando != 'f'); // finalizar execução
-    tabela.imprime();
-    cout << endl;
     return 0;
 }
